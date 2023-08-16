400ccd0cab8ab07f71bcd76732f485007c8ce14c
florencialauzz <florencialauz@gmail.com>
2023-08-16 14:27:38 -0300

    Tarea 01, decimal a binario,binario a decimal

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,26 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let sum = 0; 
+   let array = num.split("").reverse(); 
+   
+   for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i] * Math.pow(2, i); 
+   }
+   return sum;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let binario = []; 
+
+   while (num >= 1) {
+      let res = Math.trunc(num % 2); 
+   
+      num = num / 2;
+      binario.unshift(res);
+    }
+     return binario.join("");
+}
 
 module.exports = {
    BinarioADecimal,
