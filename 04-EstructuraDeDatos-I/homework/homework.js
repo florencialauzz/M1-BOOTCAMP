f728673af6c326254207079b7a4452a17125b6e0
florencialauzz <florencialauz@gmail.com>
2023-08-31 23:26:05 -0300

    Factorial, Fibonacci, Queue

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -14,9 +14,48 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+//Opcion iterativa 
+//function nFsctorialIterativo(n) {
+//   let factorial = n;
 
-function nFibonacci(n) {}
+//   while (n > 1) {
+
+//     n--;
+//     factorial *= n;   5 * 4
+//   }
+//   return factorial;
+// }
+
+//
+
+function nFactorial(n) {
+  if (n < 0) return 1; 
+  if (n === 1 || n === 0) {
+    return 1; 
+  } else {
+    return n * nFactorial(n-1); 
+  }
+}
+
+//Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+// Posicion:  0  1  2  3  4  5  6  7   8 , 9
+
+// El valor se calcula sumando los 2 numeros anteriores a el
+
+function nFibonacci(n) {
+  if (n < 0) return 0;
+  if (n === 1) return 1;
+
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
+//*                               3
+  //             fn(3)               +           fn(2)
+  //*                2               +            1
+  //           fn(2)    +    fn(1)     ||      fn(1)      +     fn(0)
+  //*           1      +      1        ||               1
+  //     fn(1) + fn(0)  ||    1                 1         +      0
+  //*          1
+  //     1     +    0
+}
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -27,7 +66,29 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+function Queue() {
+  this.fila = [];
+}
+
+Queue.prototype.enqueue = function (valor) {
+  this.fila.push(valor);
+};
+
+Queue.prototype.dequeue = function () {
+  if (this.fila.length === 0) return undefined;
+  return this.fila.shift();
+};
+
+Queue.prototype.size = function () {
+  return this.fila.length;
+};
+
+
+
+
+
+
+
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
